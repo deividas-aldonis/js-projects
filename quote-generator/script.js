fc6213e5b28f8c4c43d930fcd3e10cff6d0a3c3a
Deividas <deividas.aldonis.dev@gmail.com>
2023-05-27 18:34:54 +0300

    Add proxy server to fix CORS issues and implement fetch random quote logic

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -1,1 +1,19 @@
-console.log("hello!");
+const newQuoteBtn = document.querySelector(".new-quote-btn");
+const quoteText = document.querySelector(".quote__text");
+const quoteAuthor = document.querySelector(".quote__author");
+
+newQuoteBtn.addEventListener("click", getQuote);
+
+async function getQuote() {
+  // proxy to fix CORS issues
+  const res = await fetch("http://127.0.0.1:3000/random");
+  const data = await res.json();
+
+  const { a: author, q: quote } = data[0];
+
+  quoteText.textContent = quote;
+  quoteAuthor.textContent = author;
+}
+
+// get the first quote
+getQuote();
