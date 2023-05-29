08c59655cf8bcf115e6cb574865a85f2425883a6
Deividas <deividas.aldonis.dev@gmail.com>
2023-05-29 03:38:13 +0300

    Add picture-in-picture api

--- picture-in-picture/script.js
+++ picture-in-picture/script.js
@@ -2,6 +2,7 @@ const video = document.getElementById("video");
 const errorMessage = document.getElementById("error");
 const startCaptureBtn = document.getElementById("start-capture-btn");
 const stopCaptureBtn = document.getElementById("stop-capture-btn");
+const pipBtn = document.getElementById("pip-btn");
 
 console.error = (error) => (errorMessage.textContent = error);
 
@@ -18,6 +19,8 @@ startCaptureBtn.addEventListener("click", async () => {
       displayMediaOptions
     );
 
+    stopCaptureBtn.disabled = false;
+    pipBtn.disabled = false;
     console.error("");
   } catch (error) {
     console.error(error);
@@ -25,9 +28,24 @@ startCaptureBtn.addEventListener("click", async () => {
 });
 
 stopCaptureBtn.addEventListener("click", () => {
-  if (!video.srcObject) return;
-
   let tracks = video.srcObject.getTracks();
   tracks.forEach((track) => track.stop());
   video.srcObject = null;
+
+  stopCaptureBtn.disabled = true;
+  pipBtn.disabled = true;
+  console.error("");
+});
+
+pipBtn.addEventListener("click", async () => {
+  try {
+    if (document.pictureInPictureElement) {
+      await document.exitPictureInPicture();
+    } else {
+      await video.requestPictureInPicture();
+    }
+    console.error("");
+  } catch (error) {
+    console.error(error);
+  }
 });
