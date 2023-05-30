cb60155585fe7497c962a48c32dd162b585d787d
Deividas <deividas.aldonis.dev@gmail.com>
2023-05-30 23:21:55 +0300

    Remove a bug where background flickers if theme was set to dark before

--- light-dark-mode/script.js
+++ light-dark-mode/script.js
@@ -1,21 +1,14 @@
 const themeIcon = document.getElementById("theme-icon");
 
 function toggleTheme() {
-  const previousTheme =
-    document.documentElement.getAttribute("data-theme") || "light";
+  const previousTheme = getTheme();
   const currentTheme = previousTheme === "light" ? "dark" : "light";
   setTheme(currentTheme);
 }
 
 function setTheme(currentTheme) {
   document.documentElement.setAttribute("data-theme", currentTheme);
-
-  if (currentTheme === "light") {
-    themeIcon.src = "images/moon.svg";
-  } else {
-    themeIcon.src = "images/sun.svg";
-  }
-  themeIcon.classList.remove("hidden");
+  setIcon(currentTheme);
   saveTheme(currentTheme);
 }
 
@@ -23,21 +16,27 @@ function saveTheme(currentTheme) {
   localStorage.setItem("theme", currentTheme);
 }
 function getTheme() {
-  return localStorage.getItem("theme");
+  const theme = document.documentElement.getAttribute("data-theme");
+
+  if (theme === "light" || theme === "dark") return theme;
+
+  const prefDarkTheme = window.matchMedia(
+    "(prefers-color-scheme:dark)"
+  ).matches;
+  return prefDarkTheme ? "dark" : "light";
+}
+
+function setIcon(theme) {
+  if (theme === "light") {
+    themeIcon.src = "images/moon.svg";
+  } else {
+    themeIcon.src = "images/sun.svg";
+  }
+  themeIcon.classList.remove("hidden");
 }
 
 themeIcon.addEventListener("click", toggleTheme);
 
-window.onload = () => {
-  // Check if theme was saved in localStorage.
-  const theme = getTheme();
-  if (!theme) {
-    // Check users preferred theme
-    const prefDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-    prefDark ? setTheme("dark") : setTheme("light");
-  }
-  // If user has saved theme in localStorage
-  else {
-    setTheme(theme);
-  }
-};
+window.addEventListener("load", () => {
+  setIcon(getTheme());
+});
---
+++ light-dark-mode/theme.js
@@ -0,0 +1,28 @@
+function setTheme(theme) {
+  document.documentElement.setAttribute("data-theme", theme);
+}
+
+(function () {
+  // Check if theme was saved in localStorage.
+  const theme = localStorage.getItem("theme");
+  // If there's no theme in localStorage
+  if (!theme) {
+    // Check users preferred theme
+    const prefDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    prefDark ? setTheme("dark") : setTheme("light");
+  }
+  // If user has saved theme in localStorage
+  else if (theme) {
+    // Check if theme name is "light" or "dark"
+    if (theme === "dark" || "light") {
+      setTheme(theme);
+    }
+    // Some kind of error, theme name should be "light" or "dark"
+    else {
+      const prefDark = window.matchMedia(
+        "(prefers-color-scheme: dark)"
+      ).matches;
+      prefDark ? setTheme("dark") : setTheme("light");
+    }
+  }
+})();
