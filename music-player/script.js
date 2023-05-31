8b367c35b7bbf3edc5b77a1019eea8d43f0b1c30
Deividas <deividas.aldonis.dev@gmail.com>
2023-05-31 21:06:06 +0300

    Style the music player

--- music-player/script.js
+++ music-player/script.js
@@ -1,1 +1,5 @@
-
+const songs = [
+  { name: "Another One Bites The Dust", artist: "Queen" },
+  { name: "Drive My Car", artist: "The Beatles" },
+  { name: "Life is a Highway", artist: "Rascal Flatts" },
+];
