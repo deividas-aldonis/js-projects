1f10b3ae2c582de44acef84cc08409f904b3748b
Deividas <deividas.aldonis.dev@gmail.com>
2023-06-16 08:11:28 +0300

    Finish slider

--- slider/script.js
+++ slider/script.js
@@ -1,52 +1,112 @@
 const sliderContainer = document.getElementById("slider-container");
 const slider = document.getElementById("slider");
 
-const SLIDER_PADDING = 20;
+const MAX_DRAG = 150;
 
-let ready = false;
-let animationID = null;
+let dragging = false;
+let animationId = null;
+let currentTranslate = 0;
 let previousPosition = 0;
 let startingPosition = 0;
-let currentTranslate = 0;
+let prevDirection = null;
+let currentDirection = null;
+let direction = null;
+let currentSpeed = 0;
+let prevEvent = null;
+let currentEvent = null;
 
-function setSlider() {
-  slider.style.transform = `translateX(${-currentTranslate}px)`;
-  if (ready) requestAnimationFrame(setSlider);
+sliderContainer.addEventListener("pointerdown", pointerDown);
+
+function pointerDown(e) {
+  sliderContainer.addEventListener("pointermove", pointerMove);
+  sliderContainer.addEventListener("pointerup", pointerUp);
+  sliderContainer.setPointerCapture(e.pointerId);
+  sliderContainer.style.cursor = "grabbing";
+  slider.style.transition = "0.1s";
+
+  dragging = true;
+  startingPosition = e.clientX;
+  animationId = requestAnimationFrame(dragged);
 }
 
-function checkSliderPosition() {
-  const sliderWidth = slider.offsetWidth;
-  const sliderScrollWidth = slider.scrollWidth + SLIDER_PADDING - sliderWidth;
+function pointerMove(e) {
+  if (!dragging) return;
 
-  if (currentTranslate < 0) currentTranslate = 0;
-  if (currentTranslate > sliderScrollWidth) currentTranslate = sliderScrollWidth;
+  currentDirection = e.clientX;
+  currentEvent = e;
 
-  previousPosition = currentTranslate;
-  setSlider();
-}
+  if (prevDirection === null) prevDirection = startingPosition;
 
-function pointerDown({ pointerId, clientX }) {
-  sliderContainer.setPointerCapture(pointerId);
-  animationID = requestAnimationFrame(setSlider);
-  ready = true;
-  startingPosition = clientX;
-}
+  if (currentDirection > prevDirection) {
+    direction = "left";
+  }
 
-function pointerMove({ clientX }) {
-  if (!ready) return;
+  if (currentDirection < prevDirection) {
+    direction = "right";
+  }
 
-  const currentPosition = clientX;
+  prevDirection = currentDirection;
+
+  const currentPosition = e.clientX;
   currentTranslate = previousPosition + (startingPosition - currentPosition);
 }
 
-function pointerUp() {
-  cancelAnimationFrame(animationID);
-  checkSliderPosition();
-  ready = false;
+function pointerUp(e) {
+  sliderContainer.removeEventListener("pointermove", pointerMove);
+  sliderContainer.removeEventListener("pointerup", pointerUp);
+  cancelAnimationFrame(animationId);
+  sliderContainer.releasePointerCapture(e.pointerId);
+  dragging = false;
+
+  if (currentSpeed > 0) {
+    if (direction === "right") {
+      currentTranslate += currentSpeed;
+    }
+    if (direction === "left") {
+      currentTranslate -= currentSpeed;
+    }
+  }
+
+  const sliderWidth = computeSliderWidth();
+
+  if (currentTranslate > sliderWidth) currentTranslate = sliderWidth;
+  if (currentTranslate < 0) currentTranslate = 0;
+
+  previousPosition = currentTranslate;
+
+  slider.style.transition = "all .5s cubic-bezier(0.25,0.46,0.45,0.94)";
+  slider.style.transform = `translateX(${-currentTranslate}px)`;
+  sliderContainer.style.cursor = "grab";
 }
 
-new ResizeObserver(checkSliderPosition).observe(slider);
+function dragged() {
+  const sliderWidth = computeSliderWidth();
+  if (
+    currentTranslate < sliderWidth + MAX_DRAG &&
+    currentTranslate > 0 - MAX_DRAG
+  ) {
+    slider.style.transform = `translateX(${-currentTranslate}px)`;
+  }
+  if (dragging) animationId = requestAnimationFrame(dragged);
+  getSpeed();
+}
 
-sliderContainer.addEventListener("pointerdown", pointerDown);
-sliderContainer.addEventListener("pointermove", pointerMove);
-sliderContainer.addEventListener("pointerup", pointerUp);
+function computeSliderWidth() {
+  const style = getComputedStyle(slider);
+  const width = parseInt(style.width);
+  const padding = parseInt(style.padding);
+  const totalWidth = slider.scrollWidth;
+  return totalWidth - width + padding;
+}
+
+function getSpeed() {
+  if (prevEvent && currentEvent) {
+    const movementX = Math.abs(currentEvent.screenX - prevEvent.screenX);
+    const movementY = Math.abs(currentEvent.screenY - prevEvent.screenY);
+    const movement = Math.sqrt(movementX * movementX + movementY * movementY);
+    currentSpeed = 10 * movement;
+  }
+
+  prevEvent = currentEvent;
+  prevSpeed = currentSpeed;
+}
